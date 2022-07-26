440416449d733de200520c9ec3b40f60ce63dd72
Chetana Srinivasa <chetana.srinivas@global-csg.com>
2022-07-26 14:42:54 +0530

    removed yabby traps from gear rules page

--- fishsmartandroid/app/src/main/java/com/mobiddiction/fishsmart/Rules/FreshWaterActivity.java
+++ fishsmartandroid/app/src/main/java/com/mobiddiction/fishsmart/Rules/FreshWaterActivity.java
@@ -23,7 +23,7 @@ public class FreshWaterActivity extends AppCompatActivity {
 
     Button summarybtn, trapbtn;
     TextView listtext, fishingrules, text1, text2, text3, text4, text5, text6, text7, text8, text9, text10, text11, text12, text13, text14, text15, text16, text17, text18, text19, text20, text21, text22;
-    TextView rulestext1, rulestext2, rulestext3, rulestext4, rulestext5, hoop, shrimp, hand, yabby, tapontext;
+    TextView rulestext1, rulestext2, rulestext3, rulestext5, hoop, shrimp, hand, tapontext;
     ScrollView summaryview, trapview;
     ImageButton backbtn;
 
@@ -54,9 +54,6 @@ public class FreshWaterActivity extends AppCompatActivity {
 
         hand = findViewById(R.id.hand);
         hand.setTypeface(Typeface.createFromAsset(getAssets(), "Bariol_Regular.otf"));
-
-        yabby = findViewById(R.id.yabby);
-        yabby.setTypeface(Typeface.createFromAsset(getAssets(), "Bariol_Regular.otf"));
 
         text1 = findViewById(R.id.text1);
         text1.setTypeface(Typeface.createFromAsset(getAssets(), "Bariol_Regular.otf"));
@@ -146,9 +143,6 @@ public class FreshWaterActivity extends AppCompatActivity {
         rulestext3 = findViewById(R.id.ruleslisttext3);
         rulestext3.setTypeface(Typeface.createFromAsset(getAssets(), "Bariol_Regular.otf"));
 
-        rulestext4 = findViewById(R.id.ruleslisttext4);
-        rulestext4.setTypeface(Typeface.createFromAsset(getAssets(), "Bariol_Regular.otf"));
-
         rulestext5 = findViewById(R.id.ruleslisttext5);
         rulestext5.setTypeface(Typeface.createFromAsset(getAssets(), "Bariol_Regular.otf"));
 
@@ -189,13 +183,6 @@ public class FreshWaterActivity extends AppCompatActivity {
                 "&#8226;   Maximum set time: Zero. Must be continuously and manually propelled. Not to be set, staked or joined with any other net.<br><br>" +
                 "&#8226;   Waters: May only be used to take Yabbies in inland waters, being ground tanks, bore drains or lagoons. Not permitted in any river or public dam, or in closed areas or trout waters.<br><br>" +
                 "&#8226;   Must be used as a hand implement only."));
-
-        rulestext4.setText(Html.fromHtml("<br>&#8226;   Maximum dimensions: 1m length x 0.6m width x 0.3m depth.<br><br>" +
-                "&#8226;   Netting or mesh size: 13mm minimum measured across the stretched diagonal from knot to knot. Rigid mesh such as metal or hard plastic is prohibited.<br><br>" +
-                "&#8226;   Entrance funnels: A bycatch reduction device must be fitted to all entrance funnels. The device must consist of a rigid ring with a maximum internal diameter of 90mm permanently affixed to entry funnels at some point along their length, so as to restrict the entry funnels to a maximum opening of 90mm measured in any direction. This is to protect platypus, turtles and birds from entering the trap in search of food and getting caught in the trap.<br><br>" +
-                "&#8226;   Maximum number: Unless otherwise specified, up to five traps may be set, used or in your possession.<br><br>" +
-                "&#8226;   Maximum set time: Cannot be left set for more than 24 hours.<br><br>" +
-                "&#8226;   Identification: Position of the trap located by a buoy or tag. If the trap is identified by a buoy, the buoy must be positioned above the trap and measure not less than 100mm in all dimensions and must be 50mm above the water. Any rope attached to the buoy must not be floating on the surface of the water. If the trap is identified by a tag, the tag must be attached to the trap at or above the water level and have dimensions of at least 80mm x 45mm. Identification tags and buoys must clearly display the letters YT, initial and surname, year of birth and postcode of the person using the trap, 15mm in height, clearly visible and in a contrasting colour.<br>"));
 
         rulestext5.setText(Html.fromHtml("Waters: May be used in inland waters to take Yabbies and Freshwater Shrimp, except trout waters and closed waters, plus the following areas where platypus are found:<br><br> 1.   Waters east of the Newell Highway (except private farm dams).<br><br>" +
                 " 2.   The Murray River from the Newell Highway at Tocumwal downstream to the Echuca Road Bridge.<br><br>" +
@@ -275,16 +262,6 @@ public class FreshWaterActivity extends AppCompatActivity {
             summaryview.setVisibility(View.GONE);
             trapview.setVisibility(View.VISIBLE);
             trapview.scrollTo(0, hand.getTop());
-
-        }else if (v.getId() == R.id.text21) {
-
-            summarybtn.setSelected(false);
-            trapbtn.setSelected(true);
-            summarybtn.setTextColor(Color.parseColor("#9b9b9b"));
-            trapbtn.setTextColor(Color.parseColor("#df8871"));
-            summaryview.setVisibility(View.GONE);
-            trapview.setVisibility(View.VISIBLE);
-            trapview.scrollTo(0, yabby.getTop());
 
         }
         }
